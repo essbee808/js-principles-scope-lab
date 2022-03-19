bfe4d4b0aa4fb7fe3fe5e772a023caca440e6baa
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-18 21:03:32 -0700

    commit tree 63311adc56170c348956b3ca7fbb1f2f589a2c19

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'bobby';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    // declares a variable called bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bobby';
+}
